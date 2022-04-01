363b7d85fc8824b5aacc205bac5daec48e0ad00e
Adrian Valdes <adrianvaldeshdz@gmail.com>
2022-04-01 12:24:42 +0200

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+/**
+ * @fileoverview eslint shareable config for javascript projects
+ *
+ * @see http://eslint.org/docs/developer-guide/shareable-configs.html
+ * @see https://google.github.io/styleguide/jsguide.html
+ * @see https://prettier.io/docs/en/integrating-with-linters.html
+ */
+
+module.exports = {
+    env: {
+        amd: true,
+        browser: true,
+        node: true,
+        es6: true,
+        jest: true
+    },
+    extends: [
+        "plugin:prettier/recommended",
+        "eslint:recommended",
+        "google",
+        "prettier",
+        "plugin:react/recommended"
+    ],
+    plugins: ["import"],
+    rules: {
+        "indent": "off",
+        "require-jsdoc": 0,
+        "prettier/prettier": [
+            "error",
+            {
+                tabWidth: 4,
+                singleQuote: false,
+                trailingComma: "none",
+                bracketSpacing: false,
+                arrowParens: "always",
+                quoteProps: "consistent",
+                endOfLine: "auto"
+            }
+        ]
+    },
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        requireConfigFile: false,
+        ecmaFeatures: {
+            jsx: true
+        }
+    }
+};
